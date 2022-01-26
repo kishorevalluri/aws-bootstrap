1bb7757a01a8aa22bb7f5ee023d98f9d5751b87b
Kishore Valluri <krishnakishore.valluri@gmail.com>
2022-01-26 19:39:49 +0530

    Add CodeDeploy/CodeBuild/CodePipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
-
 STACK_NAME=awsbootstrap
 REGION=ap-south-1
-
 EC2_INSTANCE_TYPE=t2.micro 
+
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
 
+Echo $CODEPIPELINE_BUCKET
 
 # Deploys static resources
-echo -e "\n\n=========== Deploying setup.yml ==========="
+echo "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --stack-name $STACK_NAME-setup \
@@ -18,17 +21,21 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
-    
-    
+
 # Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ==========="
+echo "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --stack-name $STACK_NAME \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
     # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
